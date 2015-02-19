e9990791d6a66277e95c5e0b90a34bcd7a51acfe
Julie Johnson <john2939@email.sc.edu>
2015-02-19 14:27:59 -0500

    added some backbone

---
+++ client/js/template.js
@@ -0,0 +1,13 @@
+var template = {}
+
+    template.drinkEmotion = [
+
+    "<h3><%= word1 %></h3>",
+    "<h3><%= word2 %></h3>",
+    "<h4><%= tagline %></h4>",
+    "<h4><%= username %></h4>",
+    "<img src= <%= drink %> />"
+
+
+
+].join("");

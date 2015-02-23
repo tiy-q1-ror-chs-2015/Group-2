60eeb49a9c2ad81a192464b840acbe15d893b4bb
Chris McCandlish <cmccand@gmail.com>
2015-02-23 09:03:05 -0500

    put home link in h1, changed animation

--- client/js/other.js
+++ client/js/other.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
   $('section div').on('click', 'button', function(event){
     event.preventDefault();
     var relatedMood = '.' + $(this).attr('rel');
-    $(this).siblings('button').addClass('animate');
+    $(this).siblings('button').addClass('chosenOne');
     $(this).siblings('h2').fadeOut('slow');
     $(this).parent().siblings('h2').fadeOut('slow');
     $(this).addClass('chosenOne');

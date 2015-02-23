0a2e7747135cc3847775fe421188f5e83caf9fa6
Malachi Gray <graymd.cfi@gmail.com>
2015-02-22 20:13:04 -0500

    remove anchor tag from buttons on main page and change routes

--- client/js/main.js
+++ client/js/main.js
@@ -6,26 +6,3 @@ $(function() {
 
 
 
-$(document).ready(function(){
-
-
-
-// var drinkView = new DrinkView();
-
-// var drinkNamesView = new DrinkNameView();
-
-
-  $('section div').on('click', 'button', function(event){
-    // event.preventDefault();
-    var relatedMood = '.' + $(this).attr('rel');
-    $(this).siblings('button').addClass('animate');
-    $(this).siblings('h2').fadeOut('slow');
-    // $(this).parent().siblings('h2').fadeOut('slow');
-    $(this).addClass('chosenOne');
-    $(relatedMood).addClass('active');
-    $(relatedMood).addClass('fadein');
-    // $(relatedMood).parent('div').addClass('active');
-    // $(relatedMood).parent().siblings().removeClass('active');
-  });
-
-});
--- client/js/route.js
+++ client/js/route.js
@@ -3,14 +3,38 @@ var Router = Backbone.Router.extend({
     console.log('routes have started')
   },
   routes: {
-      "mood": "moodShow",
+      "mood_lonely": "moodShowLonely",
+      "mood_happy": "moodShowHappy",
+      "mood_inspired": "moodShowInspired",
+      "mood_dark": "moodShowDark",
       // "drink": "drink"
   },
 
-  moodShow: function() {
+  moodShowLonely: function() {
+    var self = this;
+    var drinks = new DrinkCollection();
+    drinks.fetch({data: $.param({mood: 'lonely'})}).then(function() {
+      self.loadView(new DrinkNamesView({collection: drinks}));
+    });
+  },
+  moodShowHappy: function() {
     var self = this;
     var drinks = new DrinkCollection();
     drinks.fetch({data: $.param({mood: 'happy'})}).then(function() {
+      self.loadView(new DrinkNamesView({collection: drinks}));
+    });
+  },
+  moodShowInspired: function() {
+    var self = this;
+    var drinks = new DrinkCollection();
+    drinks.fetch({data: $.param({mood: 'inspired'})}).then(function() {
+      self.loadView(new DrinkNamesView({collection: drinks}));
+    });
+  },
+  moodShowDark: function() {
+    var self = this;
+    var drinks = new DrinkCollection();
+    drinks.fetch({data: $.param({mood: 'dark'})}).then(function() {
       self.loadView(new DrinkNamesView({collection: drinks}));
     });
   },
